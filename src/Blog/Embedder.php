8c096f0b5ce229e04df25df7432f3ae576852476
Christopher Hertel <mail@christopher-hertel.de>
2024-12-13 00:56:16 +0100

    chore: clean up after bundle 0.9 release (#7)

--- src/Blog/Embedder.php
+++ src/Blog/Embedder.php
@@ -4,57 +4,25 @@ declare(strict_types=1);
 
 namespace App\Blog;
 
-use Codewithkyrian\ChromaDB\Client;
-use PhpLlm\LlmChain\Bridge\OpenAI\Embeddings;
-use PhpLlm\LlmChain\Document\Vector;
-use PhpLlm\LlmChain\Model\Response\AsyncResponse;
-use PhpLlm\LlmChain\Model\Response\VectorResponse;
-use PhpLlm\LlmChain\PlatformInterface;
+use PhpLlm\LlmChain\Document\Metadata;
+use PhpLlm\LlmChain\Document\TextDocument;
+use PhpLlm\LlmChain\Embedder as LlmChainEmbedder;
 
 final readonly class Embedder
 {
     public function __construct(
         private Loader $loader,
-        private PlatformInterface $platform,
-        private Client $chromaClient,
+        private LlmChainEmbedder $embedder,
     ) {
     }
 
     public function embedBlog(): void
     {
-        $posts = $this->loader->load();
-        $vectors = $this->createEmbeddings($posts);
-        $this->pushToChromaDB($posts, $vectors);
-    }
+        $documents = [];
+        foreach ($this->loader->load() as $post) {
+            $documents[] = new TextDocument($post->id, $post->toString(), new Metadata($post->toArray()));
+        }
 
-    /**
-     * @param Post[] $posts
-     *
-     * @return Vector[]
-     */
-    private function createEmbeddings(array $posts): array
-    {
-        $texts = array_map(fn (Post $post) => $post->toString(), $posts);
-        $response = $this->platform->request(new Embeddings(), $texts);
-
-        assert($response instanceof AsyncResponse);
-        $response = $response->unwrap();
-        assert($response instanceof VectorResponse);
-
-        return $response->getContent();
-    }
-
-    /**
-     * @param Post[]   $posts
-     * @param Vector[] $vectors
-     */
-    private function pushToChromaDB(array $posts, array $vectors): void
-    {
-        $collection = $this->chromaClient->getOrCreateCollection('symfony_blog');
-
-        $ids = array_map(fn (Post $post) => $post->id, $posts);
-        $vectors = array_map(fn (Vector $vector) => $vector->getData(), $vectors);
-
-        $collection->upsert($ids, $vectors, $posts);
+        $this->embedder->embed($documents);
     }
 }
