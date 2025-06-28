0a0e6321269c740722a2cd4c0fd9a9f4911d4dfb
valtzu <valtzu@gmail.com>
2025-06-28 23:00:20 +0300

    feat: Google structured output (#354)

---
+++ examples/google/structured-output-clock.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Agent\Agent;
+use Symfony\AI\Agent\StructuredOutput\AgentProcessor as StructuredOutputProcessor;
+use Symfony\AI\Agent\Toolbox\AgentProcessor as ToolProcessor;
+use Symfony\AI\Agent\Toolbox\Tool\Clock;
+use Symfony\AI\Agent\Toolbox\Toolbox;
+use Symfony\AI\Platform\Bridge\Google\Gemini;
+use Symfony\AI\Platform\Bridge\Google\PlatformFactory;
+use Symfony\AI\Platform\Message\Message;
+use Symfony\AI\Platform\Message\MessageBag;
+use Symfony\Component\Clock\Clock as SymfonyClock;
+use Symfony\Component\Dotenv\Dotenv;
+
+require_once dirname(__DIR__, 2).'/vendor/autoload.php';
+(new Dotenv())->loadEnv(dirname(__DIR__, 2).'/.env');
+
+if (empty($_ENV['GOOGLE_API_KEY'])) {
+    echo 'Please set the GOOGLE_API_KEY environment variable.'.\PHP_EOL;
+    exit(1);
+}
+
+$platform = PlatformFactory::create($_ENV['GOOGLE_API_KEY']);
+$model = new Gemini(Gemini::GEMINI_1_5_FLASH);
+
+$clock = new Clock(new SymfonyClock());
+$toolbox = Toolbox::create($clock);
+$toolProcessor = new ToolProcessor($toolbox);
+$structuredOutputProcessor = new StructuredOutputProcessor();
+$agent = new Agent($platform, $model, [$toolProcessor, $structuredOutputProcessor], [$toolProcessor, $structuredOutputProcessor]);
+
+$messages = new MessageBag(Message::ofUser('What date and time is it?'));
+$response = $agent->call($messages, ['response_format' => [
+    'type' => 'json_schema',
+    'json_schema' => [
+        'name' => 'clock',
+        'strict' => true,
+        'schema' => [
+            'type' => 'object',
+            'properties' => [
+                'date' => ['type' => 'string', 'description' => 'The current date in the format YYYY-MM-DD.'],
+                'time' => ['type' => 'string', 'description' => 'The current time in the format HH:MM:SS.'],
+            ],
+            'required' => ['date', 'time'],
+            'additionalProperties' => false,
+        ],
+    ],
+]]);
+
+dump($response->getContent());
