php-llm/llm-chain

feat: add Gemini Embeddings (#347)
Add Embeddings support for Google Gemini.

Only batch embedding implemented.

Related to #28

src/Platform/Bridge/Google/Embeddings/ModelClient.php
@@ -1,0 +1,71 @@
+<?php
+
+namespace PhpLlm\LlmChain\Platform\Bridge\Google\Embeddings;
+
+use PhpLlm\LlmChain\Platform\Bridge\Google\Embeddings;
+use PhpLlm\LlmChain\Platform\Exception\RuntimeException;
+use PhpLlm\LlmChain\Platform\Model;
+use PhpLlm\LlmChain\Platform\ModelClientInterface;
+use PhpLlm\LlmChain\Platform\Response\VectorResponse;
+use PhpLlm\LlmChain\Platform\ResponseConverterInterface;
+use PhpLlm\LlmChain\Platform\Vector\Vector;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+/**
+ * @author Valtteri R <valtzu@gmail.com>
+ */
+final readonly class ModelClient implements ModelClientInterface, ResponseConverterInterface
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        #[\SensitiveParameter]
+        private string $apiKey,
+    ) {
+    }
+
+    public function supports(Model $model): bool
+    {
+        return $model instanceof Embeddings;
+    }
+
+    public function request(Model $model, array|string $payload, array $options = []): ResponseInterface
+    {
+        $url = \sprintf('https://generativelanguage.googleapis.com/v1beta/models/%s:%s', $model->getName(), 'batchEmbedContents');
+        $modelOptions = $model->getOptions();
+
+        return $this->httpClient->request('POST', $url, [
+            'headers' => [
+                'x-goog-api-key' => $this->apiKey,
+            ],
+            'json' => [
+                'requests' => array_map(
+                    static fn (string $text) => array_filter([
+                        'model' => 'models/'.$model->getName(),
+                        'content' => ['parts' => [['text' => $text]]],
+                        'outputDimensionality' => $modelOptions['dimensions'] ?? null,
+                        'taskType' => $modelOptions['task_type'] ?? null,
+                        'title' => $options['title'] ?? null,
+                    ]),
+                    \is_array($payload) ? $payload : [$payload],
+                ),
+            ],
+        ]);
+    }
+
+    public function convert(ResponseInterface $response, array $options = []): VectorResponse
+    {
+        $data = $response->toArray();
+
+        if (!isset($data['embeddings'])) {
+            throw new RuntimeException('Response does not contain data');
+        }
+
+        return new VectorResponse(
+            ...array_map(
+                static fn (array $item): Vector => new Vector($item['values']),
+                $data['embeddings'],
+            ),
+        );
+    }
+}

src/Platform/Bridge/Google/PlatformFactory.php
@@ -9,6 +9,7 @@ use PhpLlm\LlmChain\Platform\Bridge\Google\Contract\MessageBagNormalizer;
 use PhpLlm\LlmChain\Platform\Bridge\Google\Contract\ToolCallMessageNormalizer;
 use PhpLlm\LlmChain\Platform\Bridge\Google\Contract\ToolNormalizer;
 use PhpLlm\LlmChain\Platform\Bridge\Google\Contract\UserMessageNormalizer;
+use PhpLlm\LlmChain\Platform\Bridge\Google\Embeddings\ModelClient;
 use PhpLlm\LlmChain\Platform\Contract;
 use PhpLlm\LlmChain\Platform\Platform;
 use Symfony\Component\HttpClient\EventSourceHttpClient;
@@ -26,8 +27,9 @@ final readonly class PlatformFactory
     ): Platform {
         $httpClient = $httpClient instanceof EventSourceHttpClient ? $httpClient : new EventSourceHttpClient($httpClient);
         $responseHandler = new ModelHandler($httpClient, $apiKey);
+        $embeddings = new ModelClient($httpClient, $apiKey);
 
-        return new Platform([$responseHandler], [$responseHandler], Contract::create(
+        return new Platform([$responseHandler, $embeddings], [$responseHandler, $embeddings], Contract::create(
             new AssistantMessageNormalizer(),
             new MessageBagNormalizer(),
             new ToolNormalizer(),

welcoMattic/ai

src/platform/src/Bridge/Google/Embeddings/ModelClient.php
@@ -1,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Platform\Bridge\Google\Embeddings;
+
+use Symfony\AI\Platform\Bridge\Google\Embeddings;
+use Symfony\AI\Platform\Exception\RuntimeException;
+use Symfony\AI\Platform\Model;
+use Symfony\AI\Platform\ModelClientInterface;
+use Symfony\AI\Platform\Response\VectorResponse;
+use Symfony\AI\Platform\ResponseConverterInterface;
+use Symfony\AI\Platform\Vector\Vector;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+/**
+ * @author Valtteri R <valtzu@gmail.com>
+ */
+final readonly class ModelClient implements ModelClientInterface, ResponseConverterInterface
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        #[\SensitiveParameter]
+        private string $apiKey,
+    ) {
+    }
+
+    public function supports(Model $model): bool
+    {
+        return $model instanceof Embeddings;
+    }
+
+    public function request(Model $model, array|string $payload, array $options = []): ResponseInterface
+    {
+        $url = \sprintf('https://generativelanguage.googleapis.com/v1beta/models/%s:%s', $model->getName(), 'batchEmbedContents');
+        $modelOptions = $model->getOptions();
+
+        return $this->httpClient->request('POST', $url, [
+            'headers' => [
+                'x-goog-api-key' => $this->apiKey,
+            ],
+            'json' => [
+                'requests' => array_map(
+                    static fn (string $text) => array_filter([
+                        'model' => 'models/'.$model->getName(),
+                        'content' => ['parts' => [['text' => $text]]],
+                        'outputDimensionality' => $modelOptions['dimensions'] ?? null,
+                        'taskType' => $modelOptions['task_type'] ?? null,
+                        'title' => $options['title'] ?? null,
+                    ]),
+                    \is_array($payload) ? $payload : [$payload],
+                ),
+            ],
+        ]);
+    }
+
+    public function convert(ResponseInterface $response, array $options = []): VectorResponse
+    {
+        $data = $response->toArray();
+
+        if (!isset($data['embeddings'])) {
+            throw new RuntimeException('Response does not contain data');
+        }
+
+        return new VectorResponse(
+            ...array_map(
+                static fn (array $item): Vector => new Vector($item['values']),
+                $data['embeddings'],
+            ),
+        );
+    }
+}

src/platform/src/Bridge/Google/PlatformFactory.php
@@ -16,6 +16,7 @@ use Symfony\AI\Platform\Bridge\Google\Contract\MessageBagNormalizer;
 use Symfony\AI\Platform\Bridge\Google\Contract\ToolCallMessageNormalizer;
 use Symfony\AI\Platform\Bridge\Google\Contract\ToolNormalizer;
 use Symfony\AI\Platform\Bridge\Google\Contract\UserMessageNormalizer;
+use Symfony\AI\Platform\Bridge\Google\Embeddings\ModelClient;
 use Symfony\AI\Platform\Contract;
 use Symfony\AI\Platform\Platform;
 use Symfony\Component\HttpClient\EventSourceHttpClient;
@@ -33,8 +34,9 @@ final readonly class PlatformFactory
     ): Platform {
         $httpClient = $httpClient instanceof EventSourceHttpClient ? $httpClient : new EventSourceHttpClient($httpClient);
         $responseHandler = new ModelHandler($httpClient, $apiKey);
+        $embeddings = new ModelClient($httpClient, $apiKey);
 
-        return new Platform([$responseHandler], [$responseHandler], Contract::create(
+        return new Platform([$responseHandler, $embeddings], [$responseHandler, $embeddings], Contract::create(
             new AssistantMessageNormalizer(),
             new MessageBagNormalizer(),
             new ToolNormalizer(),