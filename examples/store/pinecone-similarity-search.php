php-llm/llm-chain

feat: implement document loader & transformer for store indexing (#343)

examples/store/pinecone-similarity-search.php
@@ -12,6 +12,7 @@ use PhpLlm\LlmChain\Platform\Message\MessageBag;
 use PhpLlm\LlmChain\Store\Bridge\Pinecone\Store;
 use PhpLlm\LlmChain\Store\Document\Metadata;
 use PhpLlm\LlmChain\Store\Document\TextDocument;
+use PhpLlm\LlmChain\Store\Document\Vectorizer;
 use PhpLlm\LlmChain\Store\Indexer;
 use Probots\Pinecone\Pinecone;
 use Symfony\Component\Dotenv\Dotenv;
@@ -46,7 +47,8 @@ foreach ($movies as $movie) {
 
 // create embeddings for documents
 $platform = PlatformFactory::create($_ENV['OPENAI_API_KEY']);
-$indexer = new Indexer($platform, $embeddings = new Embeddings(), $store);
+$vectorizer = new Vectorizer($platform, $embeddings = new Embeddings());
+$indexer = new Indexer($vectorizer, $store);
 $indexer->index($documents);
 
 $model = new GPT(GPT::GPT_4O_MINI);

src/Store/Document/Transformer/TextSplitTransformer.php
@@ -1,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpLlm\LlmChain\Store\Document\Transformer;
+
+use PhpLlm\LlmChain\Store\Document\Metadata;
+use PhpLlm\LlmChain\Store\Document\TextDocument;
+use PhpLlm\LlmChain\Store\Document\TransformerInterface;
+use PhpLlm\LlmChain\Store\Exception\InvalidArgumentException;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * Splits a TextDocument into smaller chunks of specified size with optional overlap.
+ * If the document's content is shorter than the specified chunk size, it returns the original document as a single chunk.
+ * Overlap cannot be negative and must be less than the chunk size.
+ *
+ * @author Christopher Hertel <mail@christopher-hertel.de>
+ */
+final readonly class TextSplitTransformer implements TransformerInterface
+{
+    public const OPTION_CHUNK_SIZE = 'chunk_size';
+    public const OPTION_OVERLAP = 'overlap';
+
+    /**
+     * @param array{chunk_size?: int, overlap?: int} $options
+     */
+    public function __invoke(iterable $documents, array $options = []): iterable
+    {
+        $chunkSize = $options[self::OPTION_CHUNK_SIZE] ?? 1000;
+        $overlap = $options[self::OPTION_OVERLAP] ?? 200;
+
+        if ($overlap < 0 || $overlap >= $chunkSize) {
+            throw new InvalidArgumentException('Overlap must be non-negative and less than chunk size.');
+        }
+
+        foreach ($documents as $document) {
+            if (mb_strlen($document->content) <= $chunkSize) {
+                yield $document;
+
+                continue;
+            }
+
+            $text = $document->content;
+            $length = mb_strlen($text);
+            $start = 0;
+
+            while ($start < $length) {
+                $end = min($start + $chunkSize, $length);
+                $chunkText = mb_substr($text, $start, $end - $start);
+
+                yield new TextDocument(Uuid::v4(), $chunkText, new Metadata([
+                    'parent_id' => $document->id,
+                    'text' => $chunkText,
+                    ...$document->metadata,
+                ]));
+
+                $start += ($chunkSize - $overlap);
+            }
+        }
+    }
+}

welcoMattic/ai

examples/store/pinecone-similarity-search.php
@@ -22,6 +22,7 @@ use Symfony\AI\Platform\Message\MessageBag;
 use Symfony\AI\Store\Bridge\Pinecone\Store;
 use Symfony\AI\Store\Document\Metadata;
 use Symfony\AI\Store\Document\TextDocument;
+use Symfony\AI\Store\Document\Vectorizer;
 use Symfony\AI\Store\Indexer;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Uid\Uuid;
@@ -55,7 +56,8 @@ foreach ($movies as $movie) {
 
 // create embeddings for documents
 $platform = PlatformFactory::create($_ENV['OPENAI_API_KEY']);
-$indexer = new Indexer($platform, $embeddings = new Embeddings(), $store);
+$vectorizer = new Vectorizer($platform, $embeddings = new Embeddings());
+$indexer = new Indexer($vectorizer, $store);
 $indexer->index($documents);
 
 $model = new GPT(GPT::GPT_4O_MINI);

src/store/src/Document/Transformer/TextSplitTransformer.php
@@ -1,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\AI\Store\Document\Transformer;
+
+use Symfony\AI\Store\Document\Metadata;
+use Symfony\AI\Store\Document\TextDocument;
+use Symfony\AI\Store\Document\TransformerInterface;
+use Symfony\AI\Store\Exception\InvalidArgumentException;
+use Symfony\Component\Uid\Uuid;
+
+/**
+ * Splits a TextDocument into smaller chunks of specified size with optional overlap.
+ * If the document's content is shorter than the specified chunk size, it returns the original document as a single chunk.
+ * Overlap cannot be negative and must be less than the chunk size.
+ *
+ * @author Christopher Hertel <mail@christopher-hertel.de>
+ */
+final readonly class TextSplitTransformer implements TransformerInterface
+{
+    public const OPTION_CHUNK_SIZE = 'chunk_size';
+    public const OPTION_OVERLAP = 'overlap';
+
+    /**
+     * @param array{chunk_size?: int, overlap?: int} $options
+     */
+    public function __invoke(iterable $documents, array $options = []): iterable
+    {
+        $chunkSize = $options[self::OPTION_CHUNK_SIZE] ?? 1000;
+        $overlap = $options[self::OPTION_OVERLAP] ?? 200;
+
+        if ($overlap < 0 || $overlap >= $chunkSize) {
+            throw new InvalidArgumentException('Overlap must be non-negative and less than chunk size.');
+        }
+
+        foreach ($documents as $document) {
+            if (mb_strlen($document->content) <= $chunkSize) {
+                yield $document;
+
+                continue;
+            }
+
+            $text = $document->content;
+            $length = mb_strlen($text);
+            $start = 0;
+
+            while ($start < $length) {
+                $end = min($start + $chunkSize, $length);
+                $chunkText = mb_substr($text, $start, $end - $start);
+
+                yield new TextDocument(Uuid::v4(), $chunkText, new Metadata([
+                    'parent_id' => $document->id,
+                    'text' => $chunkText,
+                    ...$document->metadata,
+                ]));
+
+                $start += ($chunkSize - $overlap);
+            }
+        }
+    }
+}