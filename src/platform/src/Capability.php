1e06720befc4995c0f993282989a8153cfb66a7d
Mathieu Santostefano <msantostefano@proton.me>
2025-07-18 11:13:39 +0200

    Make Capability class an Enum

--- src/platform/src/Capability.php
+++ src/platform/src/Capability.php
@@ -14,23 +14,23 @@ namespace Symfony\AI\Platform;
 /**
  * @author Christopher Hertel <mail@christopher-hertel.de>
  */
-class Capability
+enum Capability: string
 {
     // INPUT
-    public const INPUT_AUDIO = 'input-audio';
-    public const INPUT_IMAGE = 'input-image';
-    public const INPUT_MESSAGES = 'input-messages';
-    public const INPUT_MULTIPLE = 'input-multiple';
-    public const INPUT_PDF = 'input-pdf';
-    public const INPUT_TEXT = 'input-text';
+    case INPUT_AUDIO = 'input-audio';
+    case INPUT_IMAGE = 'input-image';
+    case INPUT_MESSAGES = 'input-messages';
+    case INPUT_MULTIPLE = 'input-multiple';
+    case INPUT_PDF = 'input-pdf';
+    case INPUT_TEXT = 'input-text';
 
     // OUTPUT
-    public const OUTPUT_AUDIO = 'output-audio';
-    public const OUTPUT_IMAGE = 'output-image';
-    public const OUTPUT_STREAMING = 'output-streaming';
-    public const OUTPUT_STRUCTURED = 'output-structured';
-    public const OUTPUT_TEXT = 'output-text';
+    case OUTPUT_AUDIO = 'output-audio';
+    case OUTPUT_IMAGE = 'output-image';
+    case OUTPUT_STREAMING = 'output-streaming';
+    case OUTPUT_STRUCTURED = 'output-structured';
+    case OUTPUT_TEXT = 'output-text';
 
     // FUNCTIONALITY
-    public const TOOL_CALLING = 'tool-calling';
+    case TOOL_CALLING = 'tool-calling';
 }
