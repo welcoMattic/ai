d6cb1e2ca9c20db30f9e43f5f8978c5ed79ba41e
valtzu <valtzu@gmail.com>
2025-07-20 00:44:33 +0300

    [McpBundle] Wire & configure services explicitly

--- src/mcp-bundle/src/McpBundle.php
+++ src/mcp-bundle/src/McpBundle.php
@@ -14,9 +14,12 @@ namespace Symfony\AI\McpBundle;
 use Symfony\AI\McpBundle\Command\McpCommand;
 use Symfony\AI\McpBundle\Controller\McpController;
 use Symfony\AI\McpBundle\Routing\RouteLoader;
+use Symfony\AI\McpSdk\Server\NotificationHandlerInterface;
+use Symfony\AI\McpSdk\Server\RequestHandlerInterface;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 final class McpBundle extends AbstractBundle
@@ -50,21 +53,32 @@ final class McpBundle extends AbstractBundle
             return;
         }
 
+        $container->registerForAutoconfiguration(NotificationHandlerInterface::class)
+            ->addTag('mcp.server.notification_handler');
+        $container->registerForAutoconfiguration(RequestHandlerInterface::class)
+            ->addTag('mcp.server.request_handler');
+
         if ($transports['stdio']) {
             $container->register('mcp.server.command', McpCommand::class)
-                ->setAutowired(true)
+                ->setArguments([
+                    new Reference('mcp.server'),
+                ])
                 ->addTag('console.command');
         }
 
         if ($transports['sse']) {
             $container->register('mcp.server.controller', McpController::class)
-                ->setAutowired(true)
+                ->setArguments([
+                    new Reference('mcp.server'),
+                    new Reference('mcp.server.sse.store.cache_pool'),
+                    new Reference('router'),
+                ])
                 ->setPublic(true)
                 ->addTag('controller.service_arguments');
         }
 
         $container->register('mcp.server.route_loader', RouteLoader::class)
-            ->setArgument('$sseTransportEnabled', $transports['sse'])
+            ->setArgument(0, $transports['sse'])
             ->addTag('routing.route_loader');
     }
 }
