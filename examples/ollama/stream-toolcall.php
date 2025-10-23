6a9eb7056145e0cab915c2a1d7866e1304871d60
Dominic Wagner <mail@dominic-wagner.de>
2025-10-23 15:27:22 +0200

    [Platform] add ollama toolcall support for streaming

---
+++ examples/ollama/stream-toolcall.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\AI\Agent\Agent;
+use Symfony\AI\Agent\Toolbox\AgentProcessor;
+use Symfony\AI\Agent\Toolbox\Tool\Clock;
+use Symfony\AI\Agent\Toolbox\Toolbox;
+use Symfony\AI\Platform\Bridge\Ollama\PlatformFactory;
+use Symfony\AI\Platform\Message\Message;
+use Symfony\AI\Platform\Message\MessageBag;
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+$platform = PlatformFactory::create(env('OLLAMA_HOST_URL'), http_client());
+
+$toolbox = new Toolbox([new Clock()], logger: logger());
+$processor = new AgentProcessor($toolbox);
+$agent = new Agent($platform, env('OLLAMA_LLM'), [$processor], [$processor]);
+
+$messages = new MessageBag(Message::ofUser('What time is it?'));
+
+$result = $agent->call($messages, ['stream' => true]);
+
+foreach ($result->getContent() as $chunk) {
+    echo $chunk->getContent();
+}
+
+echo \PHP_EOL;
