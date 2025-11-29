53b907007d48dc8945102303f9b75464f46da0cf
Christopher Hertel <mail@christopher-hertel.de>
2025-11-29 11:01:07 +0100

    Switch to newer gemini model supporting structured + tool call

--- examples/gemini/structured-output-clock.php
+++ examples/gemini/structured-output-clock.php
@@ -30,7 +30,7 @@ $platform = PlatformFactory::create(env('GEMINI_API_KEY'), http_client(), eventD
 $clock = new Clock(new SymfonyClock());
 $toolbox = new Toolbox([$clock], logger: logger());
 $toolProcessor = new AgentProcessor($toolbox);
-$agent = new Agent($platform, 'gemini-2.5-flash', [$toolProcessor], [$toolProcessor]);
+$agent = new Agent($platform, 'gemini-3-pro-preview', [$toolProcessor], [$toolProcessor]);
 
 $messages = new MessageBag(Message::ofUser('What date and time is it?'));
 $result = $agent->call($messages, ['response_format' => [
